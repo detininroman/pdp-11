b03122bc396322240f6b0a727529aa449c685c26
Egor Zyazev <zyazev.ea@phystech.edu>
2019-11-13 01:42:30 +0300

    Moved files to pipeline folder. Changed memory variable format

--- pipeline/memory.cpp
+++ pipeline/memory.cpp
@@ -1,20 +1,20 @@
 #include "memory.h"
-#include "params.h"
+#include "../params.h"
 
 Memory::Memory(size_t size) {
-    memory_cells = new char[size];
+    memory_cells = new uint8_t[size];
 }
 
 Memory::~Memory() {
     delete memory_cells;
 }
 
-int Video::read(int pos, char &val) const{
+int Video::read(int pos, uint8_t &val) const{
     val = memory_cells[pos];
     return 0;
 }
 
-int Video::write(int pos, char val) {
+int Video::write(int pos, uint8_t val) {
     memory_cells[pos] = val;
     return 0;
 }
