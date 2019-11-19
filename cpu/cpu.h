d020219a2d47068130aedc1ced18dd096e5695ea
Egor Zyazev <zyazev.ea@phystech.edu>
2019-11-19 21:22:21 +0300

    CPU -> Pipeline architecture proposition

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -1,12 +1,19 @@
 #pragma once
 #include "../memory/memory.h"
+#include "../pipeline/pipeline.hpp"
 
 class CPU{
 public:
+    CPU(); // <---- can run this as singletone
+    void addTicks();
+    void addTicks(int);
     // INSTRUCTIONS
-
 private:
-    uint16_t* reg[7];
+    int ticks;
+    // TODO:: change according to real data
+    const int defaultTicksAddCount = 50;
+    Pipeline line;
+    uint16_t *reg[7];
     RAM RAM;
     VIDEO VRAM;
 };
--- memory/memory.h
+++ memory/memory.h
@@ -21,7 +21,7 @@ public:
     virtual int setByteValue(uint16_t pos, uint8_t val) = 0;
     virtual int getWordValue(uint16_t pos, uint16_t &val) const = 0;
     virtual int setWordValue(uint16_t pos, uint16_t val) = 0;
-    virtual uint8_t* getByteAdress(uint16_t pos) const = 0;
+    virtual uint8_t *getByteAdress(uint16_t pos) const = 0;
 protected:
     uint8_t* memory_cells;
 };
@@ -33,7 +33,7 @@ public:
     int setByteValue(uint16_t pos, uint8_t val);
     int getWordValue(uint16_t pos, uint16_t &val) const;
     int setWordValue(uint16_t pos, uint16_t val);
-    uint8_t* getByteAdress(uint16_t pos) const;
+    uint8_t *getByteAdress(uint16_t pos) const;
 };
 
 class Video : Memory{
@@ -43,7 +43,7 @@ public:
     int setByteValue(uint16_t pos, uint8_t val);
     int getWordValue(uint16_t pos, uint16_t &val) const;
     int setWordValue(uint16_t pos, uint16_t val);
-    uint8_t* getByteAdress(uint16_t pos) const;
+    uint8_t *getByteAdress(uint16_t pos) const;
 };
 
 class Reg : Memory{
@@ -60,8 +60,8 @@ class MemoryDispatcher {
     //maybe we shoukd consider interface to address any type of memory
     MemoryDispatcher(MEMORY_TYPE type);
 public:
-    uint16_t    getMemValue(uint16_t address);
-    uint16_t*    getMemAddr(uint16_t address);
+    uint16_t getMemValue(uint16_t address);
+    uint16_t *getMemAddr(uint16_t address);
 private:
     shared_ptr<Memory> device;
 };
