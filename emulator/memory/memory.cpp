082f1630a5d801e2ece7819f50601213e96383b7
Egor Zyazev <zyazev.ea@phystech.edu>
2020-01-12 19:42:24 +0300

    Switch references to pointers as we need addresses

--- emulator/memory/memory.cpp
+++ emulator/memory/memory.cpp
@@ -20,6 +20,7 @@ Memory::Memory() {
 
     memory_cells = new uint8_t[RAM_SIZE + VIDEO_SIZE + ROM_SIZE]();
 }
+
 Memory::Memory(uint8_t *memory_dump, int len) {
     memory_cells = new uint8_t[RAM_SIZE + VIDEO_SIZE + ROM_SIZE];
     // Copying to ROM part.
@@ -36,23 +37,33 @@ Memory::~Memory() {
     delete memory_cells;
 }
 
-Error Memory::getByteValue(uint16_t pos, uint8_t &val) const {
-    val = memory_cells[pos];
+Error Memory::getByteValue(uint16_t pos, uint8_t *val) const {
+    if (pos > RAM_SIZE + VIDEO_SIZE + ROM_SIZE) {
+        return Error::OUT_OF_BOUNDS;
+    }
+    val = &memory_cells[pos];
     return Error::OK;
 }
 
 Error Memory::setByteValue(uint16_t pos, uint8_t val) {
+    if (pos > RAM_SIZE + VIDEO_SIZE + ROM_SIZE) {
+        return Error::OUT_OF_BOUNDS;
+    }
     memory_cells[pos] = val;
     return Error::OK;
 }
 
-Error Memory::getWordValue(uint16_t pos, uint16_t &val) const {
+Error Memory::getWordValue(uint16_t pos, uint16_t *val) const {
     if (pos % 2) {
         throw std::runtime_error(
                 "Address must be even if you want to get whole word");
     }
     // returning in BigEndian, while we store in LittleEndian
-    val = (memory_cells[pos + 1] << 8) || (memory_cells[pos] >> 8);
+    // val = (memory_cells[pos + 1] << 8) || (memory_cells[pos] >> 8);
+    if (pos > RAM_SIZE + VIDEO_SIZE + ROM_SIZE) {
+        return Error::OUT_OF_BOUNDS;
+    }
+    val = (uint16_t*) &memory_cells[pos];
     return Error::OK;
 }
 
@@ -62,6 +73,9 @@ Error Memory::setWordValue(uint16_t pos, uint16_t val) {
 }
 
 uint8_t *Memory::getByteAdress(uint16_t pos) const {
+    if (pos > RAM_SIZE + VIDEO_SIZE + ROM_SIZE) {
+        return nullptr; // Take care
+    }
     return &memory_cells[pos];
 }
 
--- emulator/memory/memory.hpp
+++ emulator/memory/memory.hpp
@@ -37,15 +37,15 @@ public:
 
     ~Memory();
 
-    Error getByteValue(uint16_t, uint8_t &) const;
+    Error getByteValue(uint16_t, uint8_t *val) const;
 
-    Error setByteValue(uint16_t, uint8_t);
+    Error setByteValue(uint16_t, uint8_t val);
 
-    Error getWordValue(uint16_t, uint16_t &) const;
+    Error getWordValue(uint16_t, uint16_t *val) const;
 
-    Error setWordValue(uint16_t, uint16_t);
+    Error setWordValue(uint16_t, uint16_t val);
 
-    uint8_t *getByteAdress(uint16_t) const;
+    uint8_t *getByteAdress(uint16_t val) const;
 
     struct Registers registers;
 
