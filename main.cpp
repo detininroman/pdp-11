ecf3acd58e29fdade90610b51f5142a01c4d56f3
detininroman <detinin@yandex-team.ru>
2020-01-12 14:32:47 +0300

    added flags and two additional screens

--- main.cpp
+++ main.cpp
@@ -8,25 +8,37 @@
 
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(2000, 1400), "PDP-11");
+    sf::RenderWindow window(sf::VideoMode(1900, 1350), "PDP-11");
 
     sf::Font font;
     font.loadFromFile("./resources/font.ttf");
 
-    Screen screen(&window, 1000, 800, 50, 50, sf::Color(60, 63, 65));
+    Screen vRam(&window, 1050, 600, 50, 50, sf::Color(60, 63, 65));
 
-    Button emulateButton(&window, font, 400, 100, 550, 875,
+    Screen byteCodeScreen(&window, 700, 600, 1150, 50, sf::Color(60, 63, 65));
+
+    Screen disAsmScreen(&window, 700, 600, 1150, 700, sf::Color(60, 63, 65));
+
+    Button emulateButton(&window, font, 400, 100, 700, 825,
                          "Emulate", sf::Color(60, 63, 65));
-    Button stopButton(&window, font, 400, 100, 550, 1000,
+    Button stopButton(&window, font, 400, 100, 700, 950,
                       "Stop", sf::Color(60, 63, 65));
-    Button stepButton(&window, font, 400, 100, 550, 1125,
+    Button stepButton(&window, font, 400, 100, 700, 1075,
                       "Step", sf::Color(60, 63, 65));
-    Button startButton(&window, font, 400, 100, 550, 1250,
+    Button startButton(&window, font, 400, 100, 700, 1200,
                        "Start", sf::Color(60, 63, 65));
+
+    Button nFlag(&window, font, 70, 70, 700, 700, "N", sf::Color(60, 63, 65));
+    Button zFlag(&window, font, 70, 70, 810, 700, "Z", sf::Color(60, 63, 65));
+    Button vFlag(&window, font, 70, 70, 920, 700, "V", sf::Color(60, 63, 65));
+    Button cFlag(&window, font, 70, 70, 1030, 700, "C", sf::Color(60, 63, 65));
+
 //    Emulator::instance().initROM("emulator/rom.raw");
 
     auto buttons = {startButton, stopButton, stepButton,
-                    emulateButton};
+                    emulateButton, nFlag, zFlag, vFlag, cFlag};
+
+    auto screens = {vRam, byteCodeScreen, disAsmScreen};
 
     while (window.isOpen()) {
         sf::Event event;
@@ -49,12 +61,16 @@ int main() {
 
         window.clear(sf::Color(43, 43, 43));
 
-        screen.update();
+        for (auto screen: screens) {
+            screen.update();
+        }
         for (auto button : buttons) {
             button.update();
         }
 
-        screen.draw();
+        for (auto screen: screens) {
+            screen.draw();
+        }
         for (auto button : buttons) {
             button.draw();
         }
